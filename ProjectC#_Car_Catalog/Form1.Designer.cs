667af0234279f30a43acdf347a5224d9f5d79bff
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-01-09 12:37:22 -0300

    v1.1

---
+++ ProjectC#_Car_Catalog/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ProjectC__Car_Catalog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.Color.Magenta;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ford",
+            "Chevrolet",
+            "Ferrari",
+            "Audi",
+            "BMW",
+            "Gurgel"});
+            this.comboBox1.Location = new System.Drawing.Point(140, 292);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(303, 36);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Magenta;
+            this.label1.Location = new System.Drawing.Point(32, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Brand";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkMagenta;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(485, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(668, 580);
+            this.panel1.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(662, 574);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Magenta;
+            this.label3.Location = new System.Drawing.Point(32, 438);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 32);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Model";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.comboBox3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.ForeColor = System.Drawing.Color.Magenta;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(140, 438);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(303, 36);
+            this.comboBox3.TabIndex = 7;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(152, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1182, 637);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Car Catalog";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ProjectC#_Car_Catalog/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ProjectC__Car_Catalog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //comboBox1 refers to Brand Cars- refere-se às marcas dos carros
+        //comboBox3 refers to Model Cars- refere-se aos modelos dos carros
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] nomeFotosDosCarros = 
+                Directory.GetFiles(@"C:\Users\paulo\Documents\Workspaces\C#Workspace\ProjectC#_Car_Catalog\ProjectC#_Car_Catalog\Cars");
+
+            foreach(string img in nomeFotosDosCarros)
+            {
+                string[] separador = img.Split('\\');
+                string[] nomeSelecionado = separador[9].Split('.');
+                //MessageBox.Show(nomeSelecionado[0]);
+                if(comboBox3.Text == nomeSelecionado[0])
+                {
+                    pictureBox2.ImageLocation = @"C:\Users\paulo\Documents\Workspaces\C#Workspace\ProjectC#_Car_Catalog\ProjectC#_Car_Catalog\Cars\" + comboBox3.Text + "." + nomeSelecionado[1];
+                }
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox3.Text = string.Empty;
+
+            if (comboBox1.Text == "Ford")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("Galaxie 1962");
+                comboBox3.Items.Add("Mustang 1966");
+                comboBox3.Items.Add("F100 1954");
+            }
+            else if (comboBox1.Text == "Chevrolet")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("FleetLine 1941");
+                comboBox3.Items.Add("Impala 1966");
+                comboBox3.Items.Add("Malibu 1954");
+            }
+            else if (comboBox1.Text == "Ferrari")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("F330 1963");
+                comboBox3.Items.Add("225S 1952");
+            }
+            else if (comboBox1.Text == "Audi")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("920 Limousine 1939");
+                comboBox3.Items.Add("10-26 HP 1911");
+            }
+            else if (comboBox1.Text == "BMW")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("327 1939");
+                comboBox3.Items.Add("503 1953");
+            }
+            else if (comboBox1.Text == "Gurgel")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("X12 1978");
+                comboBox3.Items.Add("BR800 1989");
+            }
+        }
+    }
+}
